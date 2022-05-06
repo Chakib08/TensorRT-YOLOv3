d5ef919f50e2a0d9df501f7269a34c27ab33e3f2
Mohamed Lamine Kartobi <73436355+Chakib08@users.noreply.github.com>
2022-05-06 19:31:41 +0200

    Update get_requirements.sh

--- get_requirements.sh
+++ get_requirements.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+echo "Installing megatools package..."
+sudo apt install megatools
+
 mkdir calib_images
-mkdir calibration_cache
-mkdir darknet_weights
-mkdir onnx_models
+echo "Downloading calibration caches"
+wget "https://drive.google.com/uc?export=download&id=170uWN7AMSA6m-iUMglxs3oZpZQrJBPbb" -O calibration_cache.tar.gz
+tar xzvf calibration_cache.tar.gz calibration_cache
+rm calibration_cache.tar.gz
+
+
+echo "Downloading darknet weights..."
+megadl 'https://mega.nz/#!6Ho2mIgL!1ZxJZ_Ntm5imfNKZKH12CWNKRmstsUOZh2tAClx99hA'
+tar xzvf darknet_weights.tar.gz darknet_weights
+rm darknet_weights.tar.gz
+
+echo "Downloading onnx models..."
+megadl 'https://mega.nz/#!iOImFYhA!c8xBgOxDu2OT6AiCl0sjkRoTNNxLoJ4TyGMZ7f3jUoo'
+tar xzvf onnx_models.tar.gz onnx_models
+rm onnx_models.tar.gz
+
 mkdir tensorrt_engine
 mkdir yolov3_outputs
 mkdir yolov3_tiny_outputs
